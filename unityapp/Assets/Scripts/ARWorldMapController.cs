cfc4a3135d3fcfb1868a327831b0b9155740ddff
bluestarburst <hargreaves.bryant@gmail.com>
2022-12-04 17:36:20 -0600

    Axs

--- unityapp/Assets/Scripts/ARWorldMapController.cs
+++ unityapp/Assets/Scripts/ARWorldMapController.cs
@@ -298,11 +298,6 @@ public class ARWorldMapController : MonoBehaviour
         // create a firestore location
         var location = new GeoPoint(37.7853889, -122.4056973);
 
-
-
-
-
-
         FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
 
 
@@ -355,46 +350,7 @@ public class ARWorldMapController : MonoBehaviour
             }
         });
 
-
-
-        // var dataArray = data.ToArray();
-        // // split the dataArray into chunks of 1048487 bytes
-        // int chunkSize = 1048487;
-        // int numberOfChunks = (int)Math.Ceiling((double)dataArray.Length / chunkSize);
-        // int start = 0;
-        // int end = 0;
-
-        // for (int i = 0; i < numberOfChunks; i++)
-        // {
-        //     start = i * chunkSize;
-        //     end = (i + 1) * chunkSize;
-        //     if (end > dataArray.Length)
-        //     {
-        //         end = dataArray.Length;
-        //     }
-        //     byte[] chunk = new byte[end - start];
-        //     Array.Copy(dataArray, start, chunk, 0, end - start);
-        //     // create a firestore document
-        //     docData.Add("chunk" + i, chunk);
-
-        //     // Dictionary<string, object> docData = new Dictionary<string, object>
-        //     // {
-        //     //     { "chunk", chunk },
-        //     //     { "chunkSize", chunkSize },
-        //     //     { "numberOfChunks", numberOfChunks },
-        //     //     { "start", start },
-        //     //     { "end", end },
-        //     //     { "location", location }
-        //     // };
-        //     // // add the document to the collection
-        //     // db.Collection("worldMaps").Document("worldMap").SetAsync(docData);
-        // }
-
-
-
-        // db.Collection("maps").Document().SetAsync(docData);
-
-
+        data.Dispose();
         worldMap.Dispose();
         Log(string.Format("ARWorldMap written to {0}", path));
 
