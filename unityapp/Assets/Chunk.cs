5fcee795313cd2af77a99ac1246c14c9f50f81cc
bluestarburst <hargreaves.bryant@gmail.com>
2023-01-02 12:32:21 -0600

    b

--- unityapp/Assets/Chunk.cs
+++ unityapp/Assets/Chunk.cs
@@ -80,7 +80,7 @@ public class Chunk : MonoBehaviour
                 // poster.transform.localPosition = new Vector3((float)posterData["x"], (float)posterData["y"], (float)posterData["z"]);
 
                 // poster.transform.localRotation = new Quaternion((float)posterData["qx"], (float)posterData["qy"], (float)posterData["qz"], (float)posterData["qw"]);
-                poster.transform.localRotation = Quaternion.Euler(90, 0, 0);
+                poster.transform.localRotation = Quaternion.Euler((float)posterData["rx"], (float)posterData["ry"], (float)posterData["rz"]);
 
                 // poster.transform.localScale = new Vector3((float)posterData["sx"], (float)posterData["sy"], (float)posterData["sz"]);
 
