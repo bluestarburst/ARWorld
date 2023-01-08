47fb51e5a21ececee4c2df46bba109777f72df1f
bluestarburst <hargreaves.bryant@gmail.com>
2023-01-08 16:44:18 -0600

    sadf

--- unityapp/Assets/AddObj.cs
+++ unityapp/Assets/AddObj.cs
@@ -406,6 +406,17 @@ namespace UnityEngine.XR.ARFoundation.Samples
             {
                 if (rotating == false)
                 {
+                    trueRot = spawnedObject.transform.rotation;
+                    Vector3 eulerRot = trueRot.eulerAngles;
+                    Vector3 roundedRot = new Vector3(Mathf.Round(eulerRot.x / roundTo) * roundTo, Mathf.Round(eulerRot.y / roundTo) * roundTo, Mathf.Round(eulerRot.z / roundTo) * roundTo);
+
+                    if (previousRotation != roundedRot)
+                    {
+                        previousRotation = roundedRot;
+                        spawnedObject.transform.rotation = Quaternion.Euler(roundedRot);
+                        trueRot = Quaternion.Euler(roundedRot);
+                    }
+
                     rotating = true;
                     lastTouch = position;
                     trueRot = spawnedObject.transform.rotation;
