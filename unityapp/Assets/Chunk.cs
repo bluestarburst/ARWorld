27ac5de80e151ce9bbc977870a1eca452b069ec6
bluestarburst <hargreaves.bryant@gmail.com>
2023-01-10 14:13:14 -0600

    dsfg

--- unityapp/Assets/Chunk.cs
+++ unityapp/Assets/Chunk.cs
@@ -266,6 +266,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
         {
             GameObject obj = Importer.LoadFromFile(path);
             obj.transform.SetParent(transform);
+            
             obj.transform.position = new Vector3(x, y, z);
             obj.transform.localRotation = Quaternion.Euler(rx, ry, rz);
             obj.transform.localScale = new Vector3(sx, sy, sz);
