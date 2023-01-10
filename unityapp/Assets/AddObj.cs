4b791e6aed6b1f89710278728b30ef2964132898
bluestarburst <hargreaves.bryant@gmail.com>
2023-01-10 01:00:39 -0600

    asdf

--- unityapp/Assets/AddObj.cs
+++ unityapp/Assets/AddObj.cs
@@ -316,7 +316,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
 
                 if (File.Exists(preFilePath + url))
                 {
-                    File.Delete(preFilePath + url);
+                    // File.Delete(preFilePath + url);
                     // spawnedObject = Importer.LoadFromFile(preFilePath + url);
                     // spawnedObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                     // return;
