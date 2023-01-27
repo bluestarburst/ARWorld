815d1a4b3d9b97869252412f3ef5ee6f6f247bd0
BlueStarBurst <hargreaves.bryant@gmail.com>
2023-01-27 12:57:32 -0800

    a

--- ourworld/sandbox/LocationManager.swift
+++ ourworld/sandbox/LocationManager.swift
@@ -1,5 +1,6 @@
 import MapKit
 import CoreLocation
+import CoreMotion
 
 class LocationManager: NSObject,CLLocationManagerDelegate, ObservableObject {
     @Published var region = MKCoordinateRegion()
@@ -7,19 +8,29 @@ class LocationManager: NSObject,CLLocationManagerDelegate, ObservableObject {
     @Published var latitude = 0.0
     @Published var longitude = 0.0
     
-    var accuracy = 0.0
+    var accuracy = 1000.0
+    var altAcc = 1000.0
     
     @Published var sendDat: () -> Void = {}
     
     private let manager = CLLocationManager()
     
+    private let altimeter = CMAltimeter()
+    
     override init() {
-            super.init()
-            manager.delegate = self
-            manager.desiredAccuracy = kCLLocationAccuracyBest
-            manager.requestWhenInUseAuthorization()
-            manager.startUpdatingLocation()
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        if (CMAltimeter.isAbsoluteAltitudeAvailable()) {
+            altimeter.startAbsoluteAltitudeUpdates(to: OperationQueue.main, withHandler: {(data, error) in
+                self.altitude = data?.altitude ?? 0.0
+                self.altAcc = data?.accuracy ?? 1000.0
+            })
         }
+    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
             locations.last.map {
@@ -28,14 +39,17 @@ class LocationManager: NSObject,CLLocationManagerDelegate, ObservableObject {
                     center: CLLocationCoordinate2D(latitude: $0.coordinate.latitude, longitude: $0.coordinate.longitude),
                     span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
                 )
-                altitude = $0.altitude
+                
                 latitude = $0.coordinate.latitude
                 longitude = $0.coordinate.longitude
                 
-                if (accuracy < 25) {
+                if (accuracy < 25 && altAcc < 5) {
+                    print("Swifty up:" + String(accuracy) + "   " + String(altAcc))
                     sendDat()
                 }
-                print("Swifty acc:" + String(accuracy))
+                    
+                print("Swifty down:" + String(accuracy) + "   " + String(altAcc))
+                
             }
         }
 }
