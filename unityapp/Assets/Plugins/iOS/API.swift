bb2d4bf47d05f115a5ef8be8bd313e4051d5d40e
bluestarburst <hargreaves.bryant@gmail.com>
2023-02-04 20:55:41 -0600

    a

--- unityapp/Assets/Plugins/iOS/API.swift
+++ unityapp/Assets/Plugins/iOS/API.swift
@@ -83,6 +83,10 @@ public class UnityAPI: NativeCallsProtocol {
         sendMessage(type: "change-filter", data: [r,g,b,saturation,threshold,isColor])
     }
 
+    public func deleteObj(type: String, user: String, id: String, chunkId: String) {
+        sendMessage(type: "delete-obj", data: [type, user, id, chunkId])
+    }
+
     public func nextStepFilter() {
         sendMessage(type: "next-step-filter", data: "")
     }
